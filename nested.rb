537e5aac021d0c7a3fba460b0f18b506d8880eda
Chuk Orakwusi <chukorakwusi@gmail.com>
2020-03-08 14:54:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,7 +67,8 @@ def adding_matz
       }
     }
     
-  programmer_hash
+  programmer_hash[:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}]
+  
 return programmer_hash
 
 end
